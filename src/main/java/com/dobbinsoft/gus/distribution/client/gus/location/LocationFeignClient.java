c0c0235987e6e9e8c76c9da3e303b5490ab42a90
william.wei <william.wei@yimlink.com>
2025-10-31 14:15:45 +0800

    fix: location

--- src/main/java/com/dobbinsoft/gus/distribution/client/gus/location/LocationFeignClient.java
+++ src/main/java/com/dobbinsoft/gus/distribution/client/gus/location/LocationFeignClient.java
@@ -5,6 +5,7 @@ import com.dobbinsoft.gus.distribution.client.gus.location.model.LocationVO;
 import com.dobbinsoft.gus.distribution.config.OpenFeignConfig;
 import com.dobbinsoft.gus.web.vo.R;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
         configuration = OpenFeignConfig.class)
 public interface LocationFeignClient {
 
+    @GetMapping
     R<PageResult<LocationVO>> page(
             @RequestParam Integer pageNum,
             @RequestParam Integer pageSize,
