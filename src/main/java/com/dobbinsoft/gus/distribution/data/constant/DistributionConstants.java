e290b85b68105544742cc6f9c96a65c5de47c86d
William Wei <iotechn@gmail.com>
2025-12-08 11:55:21 +0000

    fix: payment consume stream

--- src/main/java/com/dobbinsoft/gus/distribution/data/constant/DistributionConstants.java
+++ src/main/java/com/dobbinsoft/gus/distribution/data/constant/DistributionConstants.java
@@ -5,7 +5,7 @@ public class DistributionConstants {
     // The same group can receive a message.
     public static final String GROUP = "GUS_DISTRIBUTION_GROUP";
     // queue sync
-    public static final String PAYMENT_CALLBACK_STREAM_KEY = "distribution:payment:callback:stream";
+    public static final String PAYMENT_CALLBACK_STREAM_KEY = "payment:transaction:update:event:gus-distribution";
     // location header
     public static final String LOCATION_HEADER = "X-GUS-Location-Code";
 
