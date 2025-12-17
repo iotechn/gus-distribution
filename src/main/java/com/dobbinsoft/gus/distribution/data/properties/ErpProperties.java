e1cded906dd2d2bf8a36293f98a613d22c5f2a13
William Wei <iotechn@gmail.com>
2025-12-17 12:00:17 +0000

    fix: erp properties

--- src/main/java/com/dobbinsoft/gus/distribution/data/properties/ErpProperties.java
+++ src/main/java/com/dobbinsoft/gus/distribution/data/properties/ErpProperties.java
@@ -1,12 +1,14 @@
 package com.dobbinsoft.gus.distribution.data.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Component
 @ConfigurationProperties(prefix = "gus.distribution.erp")
 public class ErpProperties {
 
